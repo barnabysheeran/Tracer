b9d743bd95fd55eaae56f4f85a99568dff53c370
Barnaby Sheeran <barnabysheeran@googlemail.com>
2018-08-06 11:21:37 +0100

    Update Scene Texture Test

---
+++ src/component/Tracer/Texture/TextureSimplex.js
@@ -0,0 +1,7 @@
+export default class TextureSimplex extends Texture {
+  constructor() {
+    super();
+  }
+
+  getValue(u, v, position) {}
+}
--- src/component/Tracer/World/SceneTextureTest.js
+++ src/component/Tracer/World/SceneTextureTest.js
@@ -17,7 +17,7 @@ export default class SceneTextureTest extends Scene {
     // Constant
     const TEXTURE_GREY = new TextureConstant(vec3.fromValues(0.8, 0.8, 0.8));
     const MATERIAL_GREY = new MaterialLambertian(TEXTURE_GREY);
-    this.addSphere(vec3.fromValues(0.0, 0.0, 1.6), 0.5, MATERIAL_GREY);
+    this.addSphere(vec3.fromValues(0.0, 0.0, -1.5), 0.5, MATERIAL_GREY);
 
     // Dialectic
     const MATERIAL_DIELECTRIC = new MaterialDielectric(1.5);
@@ -26,13 +26,13 @@ export default class SceneTextureTest extends Scene {
 
     // Metal
     const TEXTURE_METAL = new TextureConstant(vec3.fromValues(0.8, 0.8, 0.8));
-    const MATERIAL_METAL = new MaterialMetal(TEXTURE_METAL, 0.2);
-    this.addSphere(vec3.fromValues(0.0, 0.0, -1.6), 0.5, MATERIAL_METAL);
+    const MATERIAL_METAL = new MaterialMetal(TEXTURE_METAL, 0.01);
+    this.addSphere(vec3.fromValues(0.0, 0.0, 1.5), 0.5, MATERIAL_METAL);
 
     // Colours
     let total = 13;
     let progressIntervalTau = (Math.PI * 2) / total;
-    let progressInterval = 1.0 / total;
+    //let progressInterval = 1.0 / total;
 
     let radius = 0.5;
     let colour;
@@ -40,25 +40,48 @@ export default class SceneTextureTest extends Scene {
     let material;
 
     for (let i = 0; i < total; i++) {
-      colour = HSVtoRGB(progressInterval * i, 0.8, 0.8);
-
-      texture = new TextureConstant(
-        vec3.fromValues(colour.r, colour.g, colour.b)
-      );
-
       // Material
-      material = new MaterialMetal(texture, 0.1);
+      if (i == 4) {
+        colour = HSVtoRGB(1.0, 0.99, 0.99);
 
-      // Sphere
-      this.addSphere(
-        vec3.fromValues(
-          Math.sin(progressIntervalTau * i) * radius,
-          -0.39,
-          Math.cos(progressIntervalTau * i) * radius
-        ),
-        0.1,
-        material
-      );
+        texture = new TextureConstant(
+          vec3.fromValues(colour.r, colour.g, colour.b)
+        );
+
+        material = new MaterialMetal(texture, 0.1);
+
+        // Sphere
+        this.addSphere(
+          vec3.fromValues(
+            Math.sin(progressIntervalTau * i) * radius,
+            -0.39,
+            Math.cos(progressIntervalTau * i) * radius
+          ),
+          0.1,
+          material
+        );
+      } else {
+        // Sphere
+        this.addSphere(
+          vec3.fromValues(
+            Math.sin(progressIntervalTau * i) * radius,
+            -0.39,
+            Math.cos(progressIntervalTau * i) * radius
+          ),
+          0.1,
+          MATERIAL_DIELECTRIC
+        );
+
+        this.addSphere(
+          vec3.fromValues(
+            Math.sin(progressIntervalTau * i) * radius,
+            -0.39,
+            Math.cos(progressIntervalTau * i) * radius
+          ),
+          0.09,
+          MATERIAL_DIELECTRIC
+        );
+      }
     }
 
     // 'Floor'
